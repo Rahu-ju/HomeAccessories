07dd0bb3ce5c2df8e075f009991da9ee98efe31b
rahu-ju <rahu.exp@gmail.com>
2019-07-21 02:46:59 +0600

    Settings environment variable using docker compose yml file.

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(&lz2p9h!_+c_la_-^i#yd4_7+p0xch!#u!5m7jxg&$_f76l6%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
